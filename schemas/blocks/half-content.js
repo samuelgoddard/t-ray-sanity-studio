0a034530d549beaa0ffb199663365b65f5c187b9
Sam Goddard <hello@samgoddard.co.uk>
2021-07-27 20:46:37 +0100

    updated half content block

--- schemas/blocks/half-content.js
+++ schemas/blocks/half-content.js
@@ -10,16 +10,29 @@ export default {
       title: 'Heading',
     },
     {
-      name: 'externalLink',
+      name: 'externalLinkUrl',
+      type: 'url',
+      description: "The optional external link URL for this content block that appears on the left (eg: http://voyagemia.com/interview/meet-t-ray-armstrong-former-drummer-vocalist/)",
+      title: 'External Link URL',
+    },
+    {
+      name: 'externalLinkText',
       type: 'string',
-      description: "The optional external link for this content block",
-      title: 'External Link',
+      description: "If the previous field is filled in, this is the text that will display, eg: 'View on Voyagemia'",
+      title: 'External Link Text',
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: 'image',
+      description: "The optional image that will appear in the left column",
     },
     {
       title: 'Content',
       name: 'content',
       type: 'array', 
       of: [{type: 'block'}],
+      validation: Rule => Rule.required()
     },
   ]
 }
