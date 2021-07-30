cf8098d9edaff848e10fbbb2677d9d36fd602137
Sam Goddard <hello@samgoddard.co.uk>
2021-07-30 15:07:53 +0100

    order plugin added to music videos

--- schemas/musicVideos.js
+++ schemas/musicVideos.js
@@ -4,6 +4,12 @@ export default {
   type: "document",
   fields: [
     {
+      name: "order",
+      title: "Order",
+      type: "number",
+      hidden: true,
+    },
+    {
       title: "Title",
       description: "The name of this music video",
       name: "title",
